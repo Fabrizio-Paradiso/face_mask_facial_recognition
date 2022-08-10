33a330246a60c84fb2a851b892d05bf2c0ae5b4d
fabriparadiso <fabrizioparadisom@gmail.com>
2022-08-10 20:03:42 -0300

    mask and recognition classes implemented

--- main.py
+++ main.py
@@ -3,74 +3,14 @@ from libraries.common import add_date_to_frame, add_match_to_frame, create_bound
 from libraries.hog import Hog
 from libraries.face_mask import FaceMask
 from libraries.face_recognition import FaceRecognition
+from libraries.mask import Mask
+from libraries.recognition import Recognition
 from libraries.soup import Soup
 from libraries.webcam import Camera
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
+
 import cv2
-import numpy
-import os
-import pandas as pd
 import sys
 import webbrowser
-
-class Mask():
-    def __init__(self) -> None:
-        self.svm_model_path = os.path.normpath(f"models/face_mask.xml")
-        self.face_mask: FaceMask = FaceMask ()
-        self.hog: Hog = Hog()
-
-        self.model =  self.hog.load_svm_model (self.svm_model_path)
-
-        self.mask = pd.read_csv('models/mask.csv')
-        self.mask.columns = [*self.mask.columns[:-1], 'Target Class']
-
-        self.scaler = StandardScaler()
-        self.scaler.fit(self.mask.drop('Target Class', axis=1).values)
-        self.scaled_data = self.scaler.transform(self.mask.drop('Target Class',axis=1).values)
-
-        self.pca = PCA(n_components=2)
-        self.pca.fit(self.scaled_data)
-
-    def mask_crop (self, faces: tuple, gray_image: numpy.ndarray) -> numpy.ndarray:
-        coordinates_roi = self.face_mask.geometrical_face_model_roi_coordinates(faces)
-        crop_image = self.face_mask.resize_crop_roi(coordinates_roi, gray_image)
-        return crop_image
-
-    def mask_prediction(self, hog_features: tuple) -> int:
-        scaled_features = self.scaler.transform(hog_features)
-        pca_features = self.pca.transform(scaled_features)
-        prediction = self.model.predict(pca_features)
-        return prediction
-
-class Recognition():
-    def __init__(self) -> None:
-        self.svm_model_path = os.path.normpath(f"models/face_recognition.xml")
-
-        self.face_recognition: FaceRecognition = FaceRecognition()
-        self.hog: Hog = Hog()
-        self.model = self.hog.load_svm_model (self.svm_model_path)
-
-        self.recognition = pd.read_csv('models/recognition.csv')
-        self.recognition.columns = [*self.recognition.columns[:-1], 'Target Class']
-
-        self.scaler = StandardScaler()
-        self.scaler.fit(self.recognition.drop('Target Class', axis=1).values)
-        self.scaled_data = self.scaler.transform(self.recognition.drop('Target Class',axis=1).values)
-
-        self.pca = PCA(n_components=66)
-        self.pca.fit(self.scaled_data)
-
-    def recognition_crop(self, faces: tuple, gray_image: numpy.ndarray) -> numpy.ndarray:
-        coordinates_roi = self.face_recognition.get_face_recognition_roi_coordinates(faces)
-        crop_image = self.face_recognition.resize_crop_roi(coordinates_roi, gray_image)
-        return crop_image
-
-    def recognition_prediction(self, hog_features: tuple) -> int:
-        scaled_features = self.scaler.transform(hog_features)
-        pca_features = self.pca.transform(scaled_features)
-        prediction = self.model.predict(pca_features)
-        return prediction
 
 mask: Mask = Mask()
 recognition: Recognition = Recognition()
@@ -89,9 +29,6 @@ if sys.argv[1]=="pc":
 if sys.argv[1]=="raspberry":
     from libraries.camera_raspberry import Camera
     ip_address = get_ip_address_raspberry()
-
-svm_model =  hog.load_svm_model(os.path.normpath(f"models/face_mask_model.xml"))
-svm_model_multiclass = hog.load_svm_model(os.path.normpath(f"models/face_recognition_model.xml"))
 
 def generator(cam):
     while True:
