08318c04647d635ea1a7b2fca0b4432338c307fb
Fabrizio Paradiso <fabrizioparadisom@gmail.com>
2022-08-14 23:13:08 -0300

    new opencv camera implementation

--- libraries/camera_opencv.py
+++ libraries/camera_opencv.py
@@ -1,27 +1,43 @@
+from libraries.base_camera import BaseCamera
 import cv2
+import os
+import numpy
 
-class Camera():
-    def __init__(self) -> None:
-        self.frame = cv2.VideoCapture(0)
+class Camera(BaseCamera):
+    video_source = 0
 
-    def get_frame(self):
+    def __init__(self):
+        if os.environ.get('OPENCV_CAMERA_SOURCE'):
+            Camera.set_video_source(int(os.environ['OPENCV_CAMERA_SOURCE']))
+        super(Camera, self).__init__()
+
+    @staticmethod
+    def set_video_source(source: int) -> None:
         """
-        This function returns the frame captured by Web Cam in numpy format
+        This function set video source for OpenCV Cam
 
         Args:
-            self: Camera(object)
-        Return:
-            frame (numpy.ndarray)
-        """
-        return self.frame.read()[1]
-
-    def frame_release(self) -> None:
-        """
-        This function indicates that the thread should be stopped
-
-        Args:
-            self: Camera(object)
+            None
         Return:
             None
         """
-        self.frame.release()
+        Camera.video_source = source
+
+    @staticmethod
+    def frames() -> numpy.ndarray:
+        """
+        This function is in charge of return the current frame from the OpenCV cam.
+
+        Args:
+            None
+        Return:
+            frame (numpy.ndarray): Current frame
+        """
+        camera = cv2.VideoCapture(Camera.video_source)
+        if not camera.isOpened():
+            raise RuntimeError('Could not start camera.')
+
+        while True:
+            # read current frame
+            _, frame = camera.read()
+            yield frame
